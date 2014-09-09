9a1dcf6fc0a26b806a94dace8af7f25a0297a84f
Alexander Häger <me@haeger.me>
2014-09-09 12:35:10 +0200

    All things in place.

--- lab1/lab1.pl
+++ lab1/lab1.pl
@@ -3,6 +3,7 @@ beautiful(ulrika).
 
 % 2
 beautiful(nisse).
+rich(nisse).
 
 % 3
 rich(bettan).
@@ -19,6 +20,7 @@ strong(bosse).
 % 6
 man(peter).
 man(bosse).
+man(nisse).
 woman(ulrika).
 woman(bettan).
 like(X, Y) :- 
@@ -28,6 +30,23 @@ like(X, Y) :-
 happy(X) :- 
     rich(X), man(X).
 happy(X) :-
-    rich(X), woman(Y).
+    rich(X), woman(X).
 
 % 8
+happy(X) :-
+    man(X), woman(Y), like(X,Y), like(Y,X).
+
+% 9
+happy(X) :-
+    woman(X), man(Y), like(X,Y), like(Y,X).
+
+% 10
+like(nisse, X) :-
+    woman(X), like(X, nisse).
+
+% 11
+like(ulrika, X) :-
+    man(X), like(X, ulrika), rich(X), kind(X).
+
+like(ulrika, X) :-
+    man(X), like(X, ulrika), beautiful(X), strong(X).
