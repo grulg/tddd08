07f6170a8e1fc0d913dada6ac831d877753a2f6f
Alexander Häger <me@haeger.me>
2014-09-09 13:18:32 +0200

    Lab 1 kind of done. Queries still get overflow. Need to ask assistant.

--- lab1/lab1.pl
+++ lab1/lab1.pl
@@ -1,52 +1,74 @@
-% 1
-beautiful(ulrika).
+/*
+ * TDDD08 Logic Programming
+ * Autumn 2014
+ * Lab 1
+ * Alexander Häger (aleha209)
+ * Emil Tholin (emith809)
+ */
 
-% 2
-beautiful(nisse).
-rich(nisse).
+% --------- Facts -----------
+woman(ulrika).
+woman(bettan).
 
-% 3
-rich(bettan).
-strong(bettan).
-
-% 4
-strong(peter).
-beautiful(peter).
-
-% 5
-kind(bosse).
-strong(bosse).
-
-% 6
 man(peter).
 man(bosse).
 man(nisse).
-woman(ulrika).
-woman(bettan).
+
+beautiful(ulrika).
+beautiful(nisse).
+beautiful(peter).
+
+rich(nisse).
+rich(bettan).
+
+strong(bettan).
+strong(peter).
+strong(bosse).
+
+kind(bosse).
+
+% --------- Procedures -----------
+
+
 like(X, Y) :- 
-    man(X), beautiful(Y), woman(Y).
+    man(X), 
+    beautiful(Y), 
+    woman(Y).
 
-% 7
-happy(X) :- 
-    rich(X), man(X).
-happy(X) :-
-    rich(X), woman(X).
-
-% 8
-happy(X) :-
-    man(X), woman(Y), like(X,Y), like(Y,X).
-
-% 9
-happy(X) :-
-    woman(X), man(Y), like(X,Y), like(Y,X).
-
-% 10
 like(nisse, X) :-
-    woman(X), like(X, nisse).
-
-% 11
-like(ulrika, X) :-
-    man(X), like(X, ulrika), rich(X), kind(X).
+    woman(X), 
+    like(X, nisse).
 
 like(ulrika, X) :-
-    man(X), like(X, ulrika), beautiful(X), strong(X).
+    man(X), 
+    like(X, ulrika), 
+    rich(X), 
+    kind(X).
+
+like(ulrika, X) :-
+    man(X), 
+    like(X, ulrika), 
+    beautiful(X), 
+    strong(X).
+
+
+
+happy(X) :- 
+    rich(X), 
+    man(X).
+
+happy(X) :-
+    rich(X),
+    woman(X).
+
+happy(X) :-
+    man(X), 
+    woman(Y), 
+    like(X,Y), 
+    like(Y,X).
+
+happy(X) :-
+    woman(X), 
+    man(Y), 
+    like(X,Y), 
+    like(Y,X).
